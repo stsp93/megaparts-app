b5d1c44215b1d15bebaf6cc7cca448d14ca634b4
stsp93 <stspsp@gmail.com>
2023-09-05 10:43:43 +0300

    carousels added

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ $("#banners-carousel").swipe({
         if (direction == 'left') $(this).carousel('next');
         if (direction == 'right') $(this).carousel('prev');
     },
-    allowPageScroll: "vertical" 
+    allowPageScroll: "vertical"
 });
 
-$('.filter').on('click', function() {
+$('.filter').on('click', function () {
     $('.search-checkboxes').toggle();
-})
+});
+
+$('.main-carousel').flickity({
+    cellAlign: 'left',
+    groupCells: true,
+    pageDots: false,
+    setGallerySize: false,
+    contain: true
+  });
+ 
+  $('.auto-carousel').flickity({
+    cellAlign: 'left',
+    groupCells: true,
+    pageDots: false,
+    autoPlay: true,
+    setGallerySize: false,
+    contain: true
+  });
+
+
+
+
